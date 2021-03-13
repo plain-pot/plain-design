9564f6d183c4539d8927ba95795d72cb06bb4aae
韦胜健 <martsforever@aliyun.com>
2021-03-14 00:52:06 +0800

    project: 准备实现icon

--- story/pages/normal/DemoIcon.tsx
+++ story/pages/normal/DemoIcon.tsx
@@ -1,13 +1,16 @@
 import {designComponent} from "plain-design-composition";
 import React from "react";
 import {DemoRow} from "../../components/DemoRow";
+import {PlIcon} from "../../../src/packages/PlIcon/PlIcon";
 
 export default designComponent({
     setup() {
         return () => (
             <div>
                 <DemoRow title={'基本用法'}>
-                    demo-icon
+                    <span>普通的文本：NORMAL normal</span>
+                    <PlIcon icon="el-icon-bell"/>
+                    <PlIcon icon="el-icon-bottom"/>
                 </DemoRow>
             </div>
         )
