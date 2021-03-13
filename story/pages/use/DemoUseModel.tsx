24b0382134d9629e1c08a02a712d959b6d535dba
韦胜健 <martsforever@aliyun.com>
2021-03-13 21:35:31 +0800

    project: add demo page

---
+++ story/pages/use/DemoUseModel.tsx
@@ -0,0 +1,12 @@
+import {designComponent} from "plain-design-composition";
+import React from "react";
+
+export default designComponent({
+    setup() {
+        return () => (
+            <div>
+                demo use model
+            </div>
+        )
+    },
+})
