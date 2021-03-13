0ad6e1ba41f8ab5725c86e6dad0ad7a56ea762b4
韦胜健 <martsforever@aliyun.com>
2021-03-13 23:16:00 +0800

    project: menu styles

--- story/app/AppMenu.tsx
+++ story/app/AppMenu.tsx
@@ -1,11 +1,21 @@
 import {designComponent} from "plain-design-composition";
 import React from "react";
+import {Menu} from "./menu.utils";
 
 export const AppMenu = designComponent({
     setup() {
         return () => (
             <div className="app-menu">
-                this is app menu
+                {Menu.data.map(group => <React.Fragment key={group.name}>
+                    <div className="app-menu-group" key={`group_${group.name}`}>
+                        <span>{group.name}</span>
+                    </div>
+                    {group.children.map(menu => (
+                        <div className="app-menu-item" key={`item_${menu.page}`}>
+                            <span>{menu.name}</span>
+                        </div>
+                    ))}
+                </React.Fragment>)}
             </div>
         )
     },
