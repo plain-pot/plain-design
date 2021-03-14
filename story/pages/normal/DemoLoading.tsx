376f0e42a9476fd14bcf9eb64885a3425a913154
韦胜健 <martsforever@aliyun.com>
2021-03-14 16:16:17 +0800

    project: 继承props

--- story/pages/normal/DemoLoading.tsx
+++ story/pages/normal/DemoLoading.tsx
@@ -20,12 +20,12 @@ export default designComponent({
                     <PlLoading type={'ice'}/>
                 </DemoRow>
                 <DemoRow title={"字体大小"}>
-                    <div style={{fontSize: '24px'}}>
-                        <PlLoading type={'alpha'}/>
-                        <PlLoading type={'beta'}/>
-                        <PlLoading type={'gamma'}/>
-                        <PlLoading type={'delta'}/>
-                        <PlLoading type={'ice'}/>
+                    <div>
+                        <PlLoading type={'alpha'} style={{fontSize: '24px'}}/>
+                        <PlLoading type={'beta'} style={{fontSize: '24px'}}/>
+                        <PlLoading type={'gamma'} style={{fontSize: '24px'}}/>
+                        <PlLoading type={'delta'} style={{fontSize: '24px'}}/>
+                        <PlLoading type={'ice'} style={{fontSize: '24px'}}/>
                     </div>
                 </DemoRow>
                 <DemoRow title={"状态颜色"}>
