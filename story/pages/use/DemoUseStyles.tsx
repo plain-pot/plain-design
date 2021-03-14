9580e6606fd821dad07bce5a2a4b21d06a1dc993
韦胜健 <martsforever@aliyun.com>
2021-03-15 07:59:05 +0800

    project: react-transition-group 提供的类型有问题

--- story/pages/use/DemoUseStyles.tsx
+++ story/pages/use/DemoUseStyles.tsx
@@ -1,12 +1,18 @@
-import {designComponent} from "plain-design-composition";
-import React from "react";
+import React, {useState} from "react";
+import {CSSTransition} from 'react-transition-group'
 
-export default designComponent({
-    setup() {
-        return () => (
-            <div>
-                demo use styles
-            </div>
-        )
-    },
-})
+export default function App() {
+    const [inProp, setInProp] = useState(false);
+    return (
+        <div>
+            <CSSTransition in={inProp} timeout={200} classNames="my-node">
+                <div>
+                    {"I'll receive my-node-* classes"}
+                </div>
+            </CSSTransition>
+            <button type="button" onClick={() => setInProp(true)}>
+                Click to Enter
+            </button>
+        </div>
+    );
+}
