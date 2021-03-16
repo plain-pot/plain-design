f8c9dce0715af25ac989abefa42eb6a53e1efdf3
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-16 10:29:05 +0800

    composition: button status

--- story/pages/normal/DemoButton.tsx
+++ story/pages/normal/DemoButton.tsx
@@ -2,6 +2,7 @@ import {designComponent} from "plain-design-composition";
 import React from "react";
 import {DemoRow} from "../../components/DemoRow";
 import {PlButton} from "../../../src/packages/PlButton/PlButton";
+import {StoryStatus} from "../../story.utils";
 
 export default designComponent({
     setup() {
@@ -10,6 +11,10 @@ export default designComponent({
                 <div>
                     <DemoRow title={"基本用法"}>
                         <PlButton label={"按钮"}/>
+                        <span>普通文本</span>
+                    </DemoRow>
+                    <DemoRow title={'状态'}>
+                        {StoryStatus.map(item => <PlButton status={item.status} key={item.status} label={item.label}/>)}
                     </DemoRow>
                 </div>
             )
---
+++ story/story.utils.ts
@@ -0,0 +1,7 @@
+export const StoryStatus = [
+    {label: '基础', status: 'primary' as 'primary'},
+    {label: '成功', status: 'success' as 'success'},
+    {label: '警告', status: 'warn' as 'warn'},
+    {label: '失败', status: 'error' as 'error'},
+    {label: '帮助', status: 'info' as 'info'},
+]
