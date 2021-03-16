9331b726c54a2168b5a19b38c6772465a34eb935
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-16 18:54:43 +0800

    composition: 准备用 react-motion 实现队列动画

--- story/pages/test/TestGroupTransition.tsx
+++ story/pages/test/TestGroupTransition.tsx
@@ -1,11 +1,56 @@
-import {designComponent} from "plain-design-composition";
+import {designComponent, reactive} from "plain-design-composition";
 import React from "react";
+
+
+const List = designComponent({
+    slots: ['default'],
+    setup({slots}) {
+        return () => (
+            <div>
+                {slots.default()}
+            </div>
+        )
+    },
+})
+
+const Item = designComponent({
+    slots: ['default'],
+    setup({slots}) {
+        return () => (
+            <div>
+                {slots.default()}
+            </div>
+        )
+    },
+})
 
 export default designComponent({
     setup() {
+
+        const state = reactive({
+            data: [
+                '上海',
+                '北京',
+                '广州',
+                '深圳',
+                '杭州',
+                '天津',
+                '苏州',
+                '厦门',
+                '香港'
+            ],
+        })
+
         return () => (
             <div>
                 <h1>测试队列动画</h1>
+                <List>
+                    {state.data.map(item => (
+                        <Item key={item}>
+                            {item}
+                        </Item>
+                    ))}
+                </List>
             </div>
         )
     },
