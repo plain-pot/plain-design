071ca8d6868f7079ab1b69c48fd6406005987c0b
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-17 12:18:16 +0800

    composition: 实现List，问题是这个Item怎么搞

--- src/packages/PlList/PlList.tsx
+++ src/packages/PlList/PlList.tsx
@@ -1,14 +1,38 @@
 import {designComponent} from "plain-design-composition";
-import React from "react";
+import React, {ReactElement} from "react";
 import FlipMove from "react-flip-move";
 import './PlList.scss'
+import {PropType} from "plain-design-composition/src/composition/prop-type";
 
 export const PlList = designComponent({
+    props: {
+        easing: {type: String, default: 'cubic-bezier(0.23, 1, 0.32, 1)'},
+        duration: {type: [String, Number], default: '300ms'},
+        enterAnimation: {type: String as PropType<FlipMove.AnimationProp>, default: 'accordionVertical'},
+        leaveAnimation: {type: String as PropType<FlipMove.AnimationProp>, default: 'accordionVertical'},
+        onStart: {type: Function as PropType<(childElement: ReactElement<any>, domNode: HTMLElement) => void>},
+        onFinish: {type: Function as PropType<(childElement: ReactElement<any>, domNode: HTMLElement) => void>},
+        onStartAll: {type: Function as PropType<(childElements: Array<ReactElement<any>>, domNodes: Array<HTMLElement>) => void>},
+        onFinishAll: {type: Function as PropType<(childElements: Array<ReactElement<any>>, domNodes: Array<HTMLElement>) => void>},
+        disableAllAnimations: {type: Boolean, default: false},
+    },
     slots: ['default'],
-    setup({slots}) {
+    setup({props, slots}) {
         return () => (
             <div className="pl-list">
-                <FlipMove typeName={null} {...{children: slots.default()} as any}/>
+                <FlipMove
+                    typeName={null}
+                    easing={props.easing}
+                    duration={props.duration}
+                    enterAnimation={props.enterAnimation}
+                    leaveAnimation={props.leaveAnimation}
+                    onStart={props.onStart}
+                    onFinish={props.onFinish}
+                    onStartAll={props.onStartAll}
+                    onFinishAll={props.onFinishAll}
+                    disableAllAnimations={props.disableAllAnimations}
+                    {...{children: slots.default()} as any}
+                />
             </div>
         )
     },
