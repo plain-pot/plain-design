2af088b2c3e7a671c7504f776047be96e8cb9bba
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-17 17:57:23 +0800

    composition: message 提示类型

--- story/pages/service/message-service.tsx
+++ story/pages/service/message-service.tsx
@@ -15,6 +15,15 @@ export default designComponent({
                 <DemoRow title={'基本用法'}>
                     <PlButton label={'显示基本消息'} onClick={() => $message('hello world' + count++)}/>
                 </DemoRow>
+                <DemoRow title={'提示类型'}>
+                    <PlButton onClick={() => $message.lite('提示信息！')} label="lite" style={{backgroundColor: 'white', border: 'solid 1px #ccc', color: '#333'}}/>
+                    <PlButton onClick={() => $message.dark('提示信息！')} label="dark" style={{backgroundColor: '#333', border: 'none'}}/>
+                    <PlButton onClick={() => $message.primary('提示信息！')} label="primary" status="primary"/>
+                    <PlButton onClick={() => $message.success('提示信息！')} label="success" status="success"/>
+                    <PlButton onClick={() => $message.warn('提示信息！')} label="warn" status="warn"/>
+                    <PlButton onClick={() => $message.error('提示信息！')} label="error" status="error"/>
+                    <PlButton onClick={() => $message.info('提示信息！')} label="help" status="info"/>
+                </DemoRow>
             </div>
         )
     },
