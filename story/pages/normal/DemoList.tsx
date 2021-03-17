7ee1e32b130c419132e5ab84a733e568e31cdccf
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-17 14:11:10 +0800

    composition: 好像几乎完成了PlList，而且不需要Item

--- story/pages/normal/DemoList.tsx
+++ story/pages/normal/DemoList.tsx
@@ -5,11 +5,20 @@ import {PlButtonGroup} from "../../../src/packages/PlButtonGroup/PlButtonGroup";
 import {PlButton} from "../../../src/packages/PlButton/PlButton";
 import {PlList} from "../../../src/packages/PlList/PlList";
 import './DemoList.scss'
+import {shuffle} from "plain-utils/object/shuffle";
 
 export default designComponent({
     setup() {
 
         let count = 0;
+
+        const animations = [
+            'elevator',
+            'fade',
+            'accordionVertical',
+            'accordionHorizontal',
+            'none',
+        ] as any[]
 
         const state = reactive({
             cities: [
@@ -19,31 +28,49 @@ export default designComponent({
                 {id: '深圳市', name: '深圳市'},
                 {id: '长沙市', name: '长沙市'},
                 {id: '南京市', name: '南京市'},
-            ]
+            ],
+            animations: animations[0],
         })
+
+        const handler = {
+            add: (index: number) => {
+                const item = state.cities[index]
+                if (!item) return
+                state.cities.splice(index + 1, 0, {
+                    id: `count_${count++}`,
+                    name: `${item.id}_${count}`,
+                })
+            },
+            remove: (index: number) => {
+                state.cities.splice(index, 1)
+            },
+        }
+
         return () => (
             <div className={'demo-list'}>
                 <DemoRow title={'基本用法，设置动画'}>
                     <PlButtonGroup>
-                        <PlButton>add</PlButton>
-                        <PlButton>remove</PlButton>
-                        <PlButton>shuffle</PlButton>
+                        <PlButton onClick={() => handler.add(0)}>add</PlButton>
+                        <PlButton onClick={() => handler.remove(0)}>remove</PlButton>
+                        <PlButton onClick={() => state.cities = shuffle(state.cities)}>shuffle</PlButton>
                     </PlButtonGroup>
                 </DemoRow>
+                <DemoRow title={'动画'}>
+                    {animations.map(ani => (
+                        <PlButton label={ani} active={state.animations === ani} key={ani} onClick={() => state.animations = ani}/>
+                    ))}
+                </DemoRow>
                 <DemoRow>
-                    <PlList>
+                    <PlList enterAnimation={state.animations} leaveAnimation={state.animations}>
                         {state.cities.map((city, index) => (
                             <div className={"test-item"} key={city.name}>
                                 <span>{city.name}</span>
                                 <div style={{position: 'absolute', right: '8px', bottom: '8px'}}>
                                     <PlButton label={'add'} status={'success'} size={'mini'} style={{marginRight: '8px'}}
-                                              onClick={() => state.cities.splice(index + 1, 0, {
-                                                  id: `count_${count++}`,
-                                                  name: `${city.id}_${count}`,
-                                              })}
+                                              onClick={() => handler.add(index)}
                                     />
                                     <PlButton label={'remove'} status={'success'} size={'mini'}
-                                              onClick={() => state.cities.splice(index, 1)}
+                                              onClick={() => handler.remove(index)}
                                     />
                                 </div>
                             </div>)
