1b21b9504ab615a988bb3840db3b055a5f4f2cf2
韦胜健 <martsforever@aliyun.com>
2021-03-18 23:48:39 +0800

    demo: complete demo color

--- story/app/menu.utils.ts
+++ story/app/menu.utils.ts
@@ -44,7 +44,7 @@ const MenuData: MenuGroup[] = [
     },
     {
         name: '基础', children: [
-            {name: 'Color', title: '颜色', page: '/normal/Color', complete: false},
+            {name: 'Color', title: '颜色', page: '/normal/DemoColor', complete: true},
             {name: 'Icon', title: '图标', page: '/normal/DemoIcon', complete: true},
             {name: 'Button', title: '按钮', page: '/normal/DemoButton', complete: true},
             {name: 'Grid', title: '栅格', page: '/normal/DemoGrid', complete: true},
