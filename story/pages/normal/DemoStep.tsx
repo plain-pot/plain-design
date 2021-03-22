8e6f95cdb1a27c0a2883ac082bb4e4df3fb2233e
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-22 10:17:32 +0800

    components: 通过绑定val切换步骤 有点问题

--- story/pages/normal/DemoStep.tsx
+++ story/pages/normal/DemoStep.tsx
@@ -9,6 +9,8 @@ import {PlStepGroup} from "../../../src/packages/PlStepGroup";
 import {PlStep} from "../../../src/packages/PlStep";
 import $$message from "../../../src/packages/$$message";
 import './DemoStep.scss'
+import PlButtonGroup from "../../../src/packages/PlButtonGroup";
+import PlButton from "../../../src/packages/PlButton";
 
 export default designPage(() => {
 
@@ -60,6 +62,7 @@ export default designPage(() => {
                     <DemoLine title={'当前步骤索引'}>
                         <PlNumber v-model={state.val[1].index}/>
                         <PlCheckbox label={'迷你尺寸'} v-model={state.val[1].mini}/>
+                        <PlCheckbox label={'纵向步骤条'} v-model={state.val[1].vertical}/>
                         <PlCheckbox label={'标题放在图标下方'} v-model={state.val[1].titleAlignBottom}/>
                         <PlCheckbox label={'显示[创建二维码]'} v-model={state.showFlag}/>
                     </DemoLine>
@@ -84,6 +87,7 @@ export default designPage(() => {
                     <DemoLine title={'当前步骤索引'}>
                         <PlNumber v-model={state.val[2].index}/>
                         <PlCheckbox label={'迷你尺寸'} v-model={state.val[2].mini}/>
+                        <PlCheckbox label={'纵向步骤条'} v-model={state.val[2].vertical}/>
                         <PlCheckbox label={'标题放在图标下方'} v-model={state.val[2].titleAlignBottom}/>
                         <PlCheckbox label={'显示[创建二维码]'} v-model={state.showFlag}/>
                     </DemoLine>
@@ -102,6 +106,97 @@ export default designPage(() => {
                             <PlStep icon="el-icon-camera-solid" title="显示二维码" content="在应用中显示二维码" onClick={() => $$message('显示二维码')}/>
                         </PlStepGroup>
                     </DemoLine>
+                </DemoRow>
+
+                <DemoRow title={'小圆点'}>
+                    <DemoLine title={'当前步骤索引'}>
+                        <PlNumber v-model={state.val[3].index}/>
+                        <PlCheckbox label={'迷你尺寸'} v-model={state.val[3].mini}/>
+                        <PlCheckbox label={'纵向步骤条'} v-model={state.val[3].vertical}/>
+                        <PlCheckbox label={'标题放在图标下方'} v-model={state.val[3].titleAlignBottom}/>
+                        <PlCheckbox label={'显示[创建二维码]'} v-model={state.showFlag}/>
+                    </DemoLine>
+                    <DemoLine>
+                        <PlStepGroup
+                            current={state.val[3].index}
+                            vertical={state.val[3].vertical}
+                            mini={state.val[3].mini}
+                            titleAlignBottom={state.val[3].titleAlignBottom}
+                            currentStatus={state.val[3].index === 5 ? 'finish' : undefined}
+                            dotIcon
+                        >
+                            <PlStep title="获取token" content="调用接口，获取token" onClick={() => $$message('获取token')}/>
+                            <PlStep title="上传logo" content="使用token上传logo图片" onClick={() => $$message('上传logo')}/>
+                            <PlStep title="创建卡券" content="调用接口创建卡券信息" onClick={() => $$message('创建卡券')}/>
+                            {state.showFlag && <PlStep title="创建二维码" content="调用接口创建二维码" onClick={() => $$message('创建二维码')}/>}
+                            <PlStep title="显示二维码" content="在应用中显示二维码" onClick={() => $$message('显示二维码')}/>
+                        </PlStepGroup>
+                    </DemoLine>
+                </DemoRow>
+                <DemoRow title={'步骤状态'}>
+                    <DemoLine>
+                        <PlStepGroup dotIcon>
+                            <PlStep title="Finish" content="This step is finish" status="finish"/>
+                            <PlStep title="Process" content="This step is processing..." status="process"/>
+                            <PlStep title="error" content="This step is error!" status="error"/>
+                            <PlStep title="Wait" content="This step is not start." status="wait"/>
+                        </PlStepGroup>
+                    </DemoLine>
+                    <DemoLine>
+                        <PlStepGroup>
+                            <PlStep title="Finish" content="This step is finish" status="finish" icon="el-icon-success"/>
+                            <PlStep title="Process" content="This step is processing..." status="process" icon="el-icon-s-tools"/>
+                            <PlStep title="error" content="This step is error!" status="error" icon="el-icon-error"/>
+                            <PlStep title="Wait" content="This step is not start." status="wait" icon="el-icon-info"/>
+                        </PlStepGroup>
+                    </DemoLine>
+                    <DemoLine>
+                        <PlStepGroup vertical>
+                            <PlStep title="Finish" content="This step is finish" status="finish"/>
+                            <PlStep title="Process" content="This step is processing..." status="process"/>
+                            <PlStep title="error" content="This step is error!" status="error"/>
+                            <PlStep title="Wait" content="This step is not start." status="wait"/>
+                        </PlStepGroup>
+                    </DemoLine>
+                    <DemoLine>
+                        <PlStepGroup dotIcon vertical>
+                            <PlStep title="Finish" content="This step is finish" status="finish"/>
+                            <PlStep title="Process" content="This step is processing..." status="process"/>
+                            <PlStep title="error" content="This step is error!" status="error"/>
+                            <PlStep title="Wait" content="This step is not start." status="wait"/>
+                        </PlStepGroup>
+                    </DemoLine>
+                </DemoRow>
+                <DemoRow title={'通过绑定val切换步骤'}>
+                    <DemoLine title={'当前步骤索引'}>
+                        <PlButtonGroup>
+                            <PlButton label={'step1'} onClick={() => state.val1[3] = 'step1'} active={state.val1[3] === 'step1'}/>
+                            <PlButton label={'step2'} onClick={() => state.val1[3] = 'step2'} active={state.val1[3] === 'step2'}/>
+                            <PlButton label={'step3'} onClick={() => state.val1[3] = 'step3'} active={state.val1[3] === 'step3'}/>
+                            <PlButton label={'step4'} onClick={() => state.val1[3] = 'step4'} active={state.val1[3] === 'step4'}/>
+                            <PlButton label={'step5'} onClick={() => state.val1[3] = 'step5'} active={state.val1[3] === 'step5'}/>
+                        </PlButtonGroup>
+                    </DemoLine>
+                    <PlStepGroup current={state.val1[3]} currentStatus={state.val1[3] === 'step5' ? 'finish' : undefined}>
+                        <PlStep title="获取token" content="调用接口，获取token" val={'step1'}/>
+                        <PlStep title="上传logo" content="使用token上传logo图片" val={'step2'}/>
+                        <PlStep title="创建卡券" content="调用接口创建卡券信息" val={'step3'}/>
+                        <PlStep title="创建二维码" content="调用接口创建二维码" val={'step4'}/>
+                        <PlStep title="显示二维码" content="在应用中显示二维码" val={'step5'}/>
+                    </PlStepGroup>
+                    <div style={{
+                        height: '100px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        border: 'dashed 1px #e1e1e1'
+                    }}>
+                        <h1 style={{display: state.val1[3] === 'step1' ? '' : 'none'}}>Step 1</h1>
+                        <h1 style={{display: state.val1[3] === 'step2' ? '' : 'none'}}>Step 2</h1>
+                        <h1 style={{display: state.val1[3] === 'step3' ? '' : 'none'}}>Step 3</h1>
+                        <h1 style={{display: state.val1[3] === 'step4' ? '' : 'none'}}>Step 4</h1>
+                        <h1 style={{display: state.val1[3] === 'step5' ? '' : 'none'}}>Step 5</h1>
+                    </div>
                 </DemoRow>
             </div>
         )
