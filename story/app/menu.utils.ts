efb69508130ceaa70801517fee7c738019b85beb
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-22 09:35:11 +0800

    components: progress 值没有变化

--- story/app/menu.utils.ts
+++ story/app/menu.utils.ts
@@ -89,7 +89,7 @@ const MenuData: MenuGroup[] = [
         name: '视图', children: [
             {name: 'List', title: '列表', page: '/normal/DemoList', complete: true},
             {name: 'VirtualList', title: '虚拟列表', page: '/normal/virtual-list', complete: false},
-            {name: 'Progress', title: '进度条', page: '/normal/progress', complete: false},
+            {name: 'Progress', title: '进度条', page: '/normal/DemoProgress', complete: true},
             {name: 'Tree', title: '树形组件', page: '/normal/tree', complete: false},
             // {name: 'Virtual Tree', title: '虚拟树', page: '/normal/virtual-tree', complete: false},
             {name: 'Pagination', title: '分页', page: '/normal/pagination', complete: false},
