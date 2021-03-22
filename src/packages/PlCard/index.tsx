7dfdf547fbec54896c059a365e7951d75bf3b8f3
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-22 12:52:07 +0800

    components: complete card

--- src/packages/PlCard/index.tsx
+++ src/packages/PlCard/index.tsx
@@ -10,8 +10,6 @@ export const PlCard = designComponent({
         width: {type: [String, Number], default: '300'},        // 卡片宽度
         noPadding: {type: Boolean},                             // 去掉标题以及内容的内边距
         mini: {type: Boolean},                                  // 小型卡片
-        title: {type: String},                                  // 卡片标题
-        desc: {type: String},                                   // 卡片标题描述
         shadow: {type: String},                                 // 阴影, normal,hover
     },
     slots: [
@@ -37,20 +35,21 @@ export const PlCard = designComponent({
             !!props.width && (style.width = unit(props.width))
         })
 
+        console.log(slots.title())
         return {
             render: () => (
                 <div className={classes.value} style={styles.value}>
                     {slots.poster.isExist() && <div className="pl-card-poster">
                         {slots.poster()}
                     </div>}
-                    {(slots.title.isExist() || slots.desc.isExist() || slots.operator.isExist() || props.title || props.desc) && (
+                    {(slots.title.isExist() || slots.desc.isExist() || slots.operator.isExist()) && (
                         <div className="pl-card-head">
                             <div className="pl-card-head-content">
-                                {(slots.title.isExist() || props.title) && <div className="pl-card-title">
-                                    {slots.title(props.title)}
+                                {slots.title.isExist() && <div className="pl-card-title">
+                                    {slots.title()}
                                 </div>}
-                                {(slots.desc.isExist() || props.desc) && <div className="pl-card-desc">
-                                    {slots.desc(props.desc)}
+                                {(slots.desc.isExist()) && <div className="pl-card-desc">
+                                    {slots.desc()}
                                 </div>}
                             </div>
                             {slots.operator.isExist() && <div className="pl-card-head-operator">
