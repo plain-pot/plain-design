49e77a9e47448fdc8c074dd73eef10fe86e4459f
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-22 09:16:12 +0800

    components: PlProgressBar

---
+++ src/packages/PlProgressBar/index.tsx
@@ -0,0 +1,81 @@
+import './progress-bar.scss'
+import {computed, designComponent} from "plain-design-composition";
+import {PROGRESS_DEFAULT_PROPS} from "./progress.utils";
+import React from 'react';
+import PlIcon from "../PlIcon";
+
+export const PlProgressBar = designComponent({
+    name: 'pl-progress-bar',
+    props: {
+        ...PROGRESS_DEFAULT_PROPS,
+
+        width: {type: String, default: '300px'},
+        height: {type: String, default: '6px'},
+        inlineText: {type: Boolean},
+    },
+    setup({props}) {
+        const iconColor = computed(() => {
+            switch (props.status) {
+                case 'success':
+                    return props.successColor
+                case 'error':
+                    return props.errorColor
+                default:
+                    return null
+            }
+        })
+
+        const outerStyles = computed(() => ({
+            height: !!props.inlineText ? '16px' : props.height,
+            width: props.width,
+            backgroundColor: props.outerColor,
+            borderRadius: props.width,
+        }))
+
+        const innerStyles = computed(() => ({
+            width: `${props.modelValue}%`,
+            backgroundColor: iconColor.value || props.innerColor,
+            borderRadius: props.width,
+        }))
+
+        return {
+            render: () => (
+                <div className="pl-progress-bar">
+                    <div className="pl-progress-bar-outer" style={outerStyles.value}>
+                        <div className="pl-progress-bar-inner" style={innerStyles.value}>
+                            {props.inlineText && props.modelValue > 20 && (
+                                <div>
+                                    <div className="pl-progress-bar-content">
+                                        {props.status === 'success' && <PlIcon icon="el-icon-check-bold" className="pl-progress-bar-icon-success" style={{color: 'white'}}/>}
+                                        {props.status === 'error' && <PlIcon icon="el-icon-close-bold" className="pl-progress-bar-icon-error" style={{color: 'white'}}/>}
+                                        {props.status !== 'success' && props.status !== 'error' && <span>{props.modelValue}%</span>}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        {props.inlineText && props.modelValue < 20 && (
+                            <div>
+                                <div className="pl-progress-bar-content">
+                                    {props.status === 'success' && <PlIcon icon="el-icon-check-bold" className="pl-progress-bar-icon-success" style={{color: props.successColor}}/>}
+                                    {props.status === 'error' && <PlIcon icon="el-icon-close-bold" className="pl-progress-bar-icon-error" style={{color: props.errorColor}}/>}
+                                    {props.status !== 'success' && props.status !== 'error' && <span>{props.modelValue}%</span>}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    {
+                        !props.inlineText && (
+                            <div className="pl-progress-bar-content">
+                                {props.status === 'success' && <PlIcon icon="el-icon-success" className="pl-progress-bar-icon-success" style={{color: props.successColor}}/>}
+                                {props.status === 'error' && <PlIcon icon="el-icon-error" className="pl-progress-bar-icon-error" style={{color: props.errorColor}}/>}
+                                {props.status !== 'success' && props.status !== 'error' && <span>{props.modelValue}%</span>}
+                            </div>
+                        )
+                    }
+                </div>
+            )
+        }
+    },
+})
+
+export default PlProgressBar
