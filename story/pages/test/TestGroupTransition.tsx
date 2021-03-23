a59f235ae48c263070dcca8f51299e0096a187c5
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-23 15:32:49 +0800

    components: 几乎完成了collapse transition

--- story/pages/test/TestGroupTransition.tsx
+++ story/pages/test/TestGroupTransition.tsx
@@ -4,27 +4,66 @@ import './TestGroupTransition.scss'
 import {PlButton} from "../../../src/packages/PlButton";
 import PlCollapseTransition from "../../../src/packages/PlCollapseTransition";
 import {DemoRow} from "../../components/DemoRow";
+import PlButtonGroup from "../../../src/packages/PlButtonGroup";
+import {shuffle} from "plain-utils/object/shuffle";
 
 export default designComponent({
     setup() {
 
         const state = reactive({
+            cities: [
+                {id: '广州市', name: '广州市'},
+                {id: '上海市', name: '上海市'},
+                {id: '北京市', name: '北京市'},
+                {id: '深圳市', name: '深圳市'},
+                {id: '长沙市', name: '长沙市'},
+                {id: '南京市', name: '南京市'},
+            ],
             val: {
                 3: true,
             } as any,
         })
 
+        let count = 0;
+        const handler = {
+            add: (index: number) => {
+                const item = state.cities[index]
+                if (!item) return
+                state.cities.splice(index + 1, 0, {
+                    id: `count_${count++}`,
+                    name: `${item.id}_${count}`,
+                })
+            },
+            remove: (index: number) => {
+                state.cities.splice(index, 1)
+            },
+        }
+
         return () => (
             <div>
+                <DemoRow>
+                    <PlButtonGroup>
+                        <PlButtonGroup>
+                            <PlButton onClick={() => handler.add(0)}>add</PlButton>
+                            <PlButton onClick={() => handler.remove(0)}>remove</PlButton>
+                            <PlButton onClick={() => state.cities = shuffle(state.cities)}>shuffle</PlButton>
+                        </PlButtonGroup>
+                    </PlButtonGroup>
+                </DemoRow>
                 <DemoRow title={'测试自动高度的外部节点'}>
                     <PlButton label={state.val[1] ? 'to hide' : 'to show'} onClick={() => state.val[1] = !state.val[1]}/>
                     <PlCollapseTransition show={state.val[1]}>
                         <div>
-                            <div style={{height: '180px', color: 'white', backgroundColor: 'blueviolet'}}>
-                                this is content
+                            <div style={{color: 'white', backgroundColor: 'blueviolet'}}>
+                                <ul>
+                                    {state.cities.map(item => (
+                                        <li key={item.id} style={{height: '28px', lineHeight: '28px'}}>{item.name}</li>
+                                    ))}
+                                </ul>
                             </div>
                         </div>
                     </PlCollapseTransition>
+                    <button>next</button>
                 </DemoRow>
                 <DemoRow title={'一开始就是显示'}>
                     <PlCollapseTransition show={true}>
