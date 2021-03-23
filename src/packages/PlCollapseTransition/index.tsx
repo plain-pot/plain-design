39be3b1a0b5d9c496631038968490ee12487798c
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-23 15:25:06 +0800

    components: 几乎完成了collapse transition

--- src/packages/PlCollapseTransition/index.tsx
+++ src/packages/PlCollapseTransition/index.tsx
@@ -14,6 +14,22 @@ class Wrapper extends React.Component<any, any> {
 
 }
 
+type CollapseElStyles = {
+    height: string,
+    paddingTop: string,
+    paddingBottom: string,
+    overflow: string,
+    display: string,
+    scrollHeight: number,
+}
+
+enum CollapseStatus {
+    show = 'show',
+    showing = 'showing',
+    hide = 'hide',
+    hiding = 'hiding',
+}
+
 export const PlCollapseTransition = designComponent({
     props: {
         show: {type: Boolean},
@@ -22,110 +38,129 @@ export const PlCollapseTransition = designComponent({
     setup({props, slots}) {
 
         const wrapperRef = useReference()
-        const freezeState = {
-            el: null as null | HTMLElement,
-            onEnd: null as null | (() => void),
 
-            show: null as null | boolean,
-            oldData: {
-                height: '',
-                paddingTop: '',
-                paddingBottom: '',
-                overflow: '',
-                display: '',
-            },
+        const state = {
+            el: null as null | HTMLElement,
+            handleTransitionEnd: null as null | (() => void),
+            status: null as null | CollapseStatus,
+            styles: null as null | CollapseElStyles,
         }
 
         const handler = {
-            onTransitionEnd: () => !!freezeState.onEnd && freezeState.onEnd(),
+            onTransitionEnd: () => !!state.handleTransitionEnd && state.handleTransitionEnd(),
             onUpdateEl: () => {
                 /*每次update之后，都检查一遍el是否发生了变化，是则重新添加事件*/
-                const {el: oldEl} = freezeState
+                const {el: oldEl} = state
                 const newEl = findDOMNode(wrapperRef.current) as HTMLElement
                 if (oldEl === newEl) { return}
                 !!oldEl && oldEl.removeEventListener('transitionend', handler.onTransitionEnd)
                 !!newEl && newEl.addEventListener('transitionend', handler.onTransitionEnd)
-                freezeState.el = newEl
+                state.el = newEl
             },
         }
 
         const methods = {
             show: async (animation = true) => {
-                const el = freezeState.el
+                const {el, status, styles} = state
                 /*没有dom元素或者已经处于显示的状态，则不做处理*/
-                if (!el || freezeState.show === true) {return}
+                if (!el || status === CollapseStatus.show || status === CollapseStatus.showing) {return}
 
-                freezeState.show = true
-                /*设置当前显示，以便读取scrollHeight高度*/
-                el.style.display = freezeState.oldData.display == 'none' ? '' : freezeState.oldData.display
-                if (animation) {
-                    /*保存要被修改的值*/
-                    freezeState.oldData = {
+                if (status !== CollapseStatus.hiding) {
+                    /*设置当前显示，以便读取scrollHeight高度*/
+                    const display = !styles ? '' : styles.display
+                    el.style.display = display === 'none' ? '' : display
+                    state.styles = {
                         height: el.style.height,
                         paddingTop: el.style.paddingTop,
                         paddingBottom: el.style.paddingBottom,
                         overflow: el.style.overflow,
-                        display: '',
+                        display: el.style.display,
+                        scrollHeight: el.scrollHeight
                     }
-                    const {scrollHeight} = el
+                } else {
+                    // hiding => showing
+                }
+                if (animation) {
+                    state.status = CollapseStatus.showing
                     /*动画开始的高度*/
                     el.style.height = '0px';
                     el.style.paddingTop = '0px';
                     el.style.paddingBottom = '0px';
                     el.style.overflow = 'hidden';
+                    // console.log('show setting')
 
                     await delay(23)
                     addClass(el, 'pl-collapse-transition');
+                    const {height, scrollHeight, paddingBottom, paddingTop, overflow} = state.styles!
                     /*动画结束的高度*/
-                    el.style.height = unit(freezeState.oldData.height || scrollHeight)!;
-                    el.style.paddingTop = freezeState.oldData.paddingTop;
-                    el.style.paddingBottom = freezeState.oldData.paddingBottom;
+                    el.style.height = unit(height || String(scrollHeight))!;
+                    el.style.paddingTop = unit(paddingTop)!;
+                    el.style.paddingBottom = unit(paddingBottom)!;
 
-                    freezeState.onEnd = async () => {
+                    state.handleTransitionEnd = async () => {
                         /*动画结束之后，将值还原*/
-                        el.style.height = freezeState.oldData.height;
-                        el.style.overflow = freezeState.oldData.overflow;
+                        el.style.height = height;
+                        el.style.overflow = overflow;
                         removeClass(el, 'pl-collapse-transition');
+                        state.status = CollapseStatus.show
                     }
+                } else {
+                    const {height, paddingBottom, paddingTop, overflow} = state.styles!
+                    el.style.height = height;
+                    el.style.paddingTop = paddingTop;
+                    el.style.paddingBottom = paddingBottom;
+                    el.style.overflow = overflow;
+                    state.status = CollapseStatus.show
                 }
             },
             hide: async (animation = true) => {
-                const el = freezeState.el
-                /*没有dom元素或者已经处于隐藏的状态，则不做处理*/
-                if (!el || freezeState.show === false) {return}
-                /*当前处于隐藏的状态*/
-                freezeState.show = false
+                const {el, status} = state
+                /*没有dom元素或者已经处于显示的状态，则不做处理*/
+                if (!el || status === CollapseStatus.hide || status === CollapseStatus.hiding) {return}
 
-                if (animation) {
-                    /*记录被修改的值*/
-                    freezeState.oldData = {
+                if (status !== CollapseStatus.showing) {
+                    state.styles = {
                         height: el.style.height,
                         paddingTop: el.style.paddingTop,
                         paddingBottom: el.style.paddingBottom,
                         overflow: el.style.overflow,
-                        display: '',
+                        display: el.style.display,
+                        scrollHeight: el.scrollHeight
                     }
-                    /*动画开始的时候的高度*/
-                    el.style.height = unit(el.scrollHeight)!;
+                } else {
+                    // hiding => showing
+                }
+                if (animation) {
+                    state.status = CollapseStatus.hiding
+                    const {height, scrollHeight, paddingTop, paddingBottom, overflow} = state.styles!
+                    /*动画开始的高度*/
+                    el.style.height = unit(height || String(scrollHeight))!;
+                    el.style.paddingTop = unit(paddingTop)!;
+                    el.style.paddingBottom = unit(paddingBottom)!;
                     el.style.overflow = 'hidden';
-                    addClass(el, 'pl-collapse-transition');
+
                     await delay(23)
+                    addClass(el, 'pl-collapse-transition');
+
                     /*动画结束的高度*/
-                    el.style.height = "0px";
-                    el.style.paddingTop = '0px'
-                    el.style.paddingBottom = '0px'
-                    freezeState.onEnd = () => {
-                        /*动画结束之后，设置display：none，并且将修改过的值复原*/
-                        el.style.height = freezeState.oldData.height;
-                        el.style.paddingTop = freezeState.oldData.paddingTop;
-                        el.style.paddingBottom = freezeState.oldData.paddingBottom;
-                        el.style.overflow = freezeState.oldData.overflow;
-                        freezeState.oldData.display = el.style.display
+                    el.style.height = '0px';
+                    el.style.paddingTop = '0px';
+                    el.style.paddingBottom = '0px';
+                    // console.log('hide setting=========================')
+
+                    state.handleTransitionEnd = async () => {
+                        /*动画结束之后，将值还原*/
+                        el.style.height = height;
+                        el.style.paddingTop = paddingTop;
+                        el.style.paddingBottom = paddingBottom;
+                        el.style.overflow = overflow;
                         el.style.display = 'none'
                         removeClass(el, 'pl-collapse-transition');
+                        state.status = CollapseStatus.hide
                     }
                 } else {
                     el.style.display = 'none'
+                    state.status = CollapseStatus.hide
                 }
             },
         }
