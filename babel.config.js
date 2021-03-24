502924570b74f220e5834e6093f0f6abc9db2769
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-24 15:56:07 +0800

    components: 添加babel插件 transform-class-properties

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
     presets: ["@babel/preset-react", "@babel/preset-env", "@babel/preset-typescript"],
     "plugins": [
         // "@babel/plugin-transform-react-jsx",
+        'transform-class-properties',
         '@babel/plugin-transform-runtime',
         JsxModel,
     ]
