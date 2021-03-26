d2de8e0f0b0d6aab25027d757c7e48bae1657392
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-26 15:34:27 +0800

    color-picker: 怎么获取组件的el对象

--- story/pages/normal/DemoColorPicker.tsx
+++ story/pages/normal/DemoColorPicker.tsx
@@ -1,4 +1,4 @@
-import {designPage} from "plain-design-composition";
+import {designPage, onMounted, useRefs} from "plain-design-composition";
 import React from "react";
 import {DemoRow} from "../../components/DemoRow";
 import {reactive} from "@vue/reactivity";
@@ -6,6 +6,9 @@ import PlColorAlphaSlider from "../../../src/packages/PlColorPicker/sub/PlColorA
 import PlColorHueSlider from "../../../src/packages/PlColorPicker/sub/PlColorHueSlider";
 import PlColorPanel from "../../../src/packages/PlColorPicker/PlColorPanel";
 import {DemoLine} from "../../components/DemoLine";
+import useColorPicker from "../../../src/packages/useColorPicker";
+import $$message from "../../../src/packages/$$message";
+import PlButton from "../../../src/packages/PlButton";
 
 export default designPage(() => {
 
@@ -15,6 +18,85 @@ export default designPage(() => {
             1: 240,
         } as any
     }).val
+
+    const {refs, onRef} = useRefs({
+        serviceBasicUsage: {} as any,
+        hexValue: {} as any,
+        rgbValue: {} as any,
+        rgbWithoutOpacity: {} as any,
+        hexWithOpacity: {} as any,
+        rgbaWithOpacity: {} as any,
+        saveValue: {} as any,
+    })
+
+    const $colorPicker = useColorPicker()
+
+    const service = {
+        serviceBasicUsage: $colorPicker({
+            reference: () => refs['serviceBasicUsage'],
+            renderAttrs: {
+                onChange: val => $$message(val)
+            },
+        }),
+        hexValue: $colorPicker({
+            reference: () => refs['hexValue'],
+            renderAttrs: {
+                onChange: val => $$message(val),
+                modelValue: '#ff0000',
+                format: 'hex',
+            },
+        }),
+        rgbValue: $colorPicker({
+            reference: () => refs['rgbValue'],
+            renderAttrs: {
+                onChange: val => $$message(val),
+                modelValue: 'rgb(134,74,212)',
+                format: 'rgb',
+            },
+        }),
+        rgbWithoutOpacity: $colorPicker({
+            reference: () => refs['rgbWithoutOpacity'],
+            renderAttrs: {
+                onChange: val => $$message(val),
+                modelValue: 'rgb(134,74,212,0.5)',
+                format: 'rgb',
+            },
+        }),
+        hexWithOpacity: $colorPicker({
+            reference: () => refs['hexWithOpacity'],
+            renderAttrs: {
+                onChange: val => $$message(val),
+                modelValue: '#00ff00',
+                format: 'hex',
+                enableAlpha: true,
+            },
+        }),
+        rgbaWithOpacity: $colorPicker({
+            reference: () => refs['rgbaWithOpacity'],
+            renderAttrs: {
+                onChange: val => $$message(val),
+                modelValue: 'rgb(134,74,212,0.5)',
+                format: 'rgb',
+                enableAlpha: true,
+            },
+        }),
+        saveValue: (() => {
+            const option = {
+                reference: () => refs['saveValue'],
+                renderAttrs: {
+                    onChange: (val: any) => option.renderAttrs.modelValue = val,
+                    modelValue: 'rgb(134,74,212,0.5)',
+                    format: 'rgb',
+                    enableAlpha: true,
+                },
+            }
+            return $colorPicker(option)
+        })()
+    }
+
+    onMounted(() => {
+        console.log(refs.serviceBasicUsage)
+    })
 
     return () => (
         <div>
@@ -48,7 +130,11 @@ export default designPage(() => {
                     </DemoLine>
                 </DemoRow>
             </DemoRow>
-
+            <DemoRow title={'Service服务'}>
+                <DemoRow title={'基本用法'}>
+                    <PlButton onClick={service.serviceBasicUsage.toggle} label={'颜色选择服务基本用法'} ref={(refer) => console.log('button', refer)}/>
+                </DemoRow>
+            </DemoRow>
         </div>
     )
 })
