4a0e9918ff75a6660d540c8243c8a432baafaa2c
韦胜健 <martsforever@aliyun.com>
2021-03-27 23:02:51 +0800

    component: 手动输入时间之后，光标会发生变化

--- story/pages/normal/DemoTime.tsx
+++ story/pages/normal/DemoTime.tsx
@@ -5,6 +5,7 @@ import PlTime from "../../../src/packages/PlTime";
 import {reactive} from "@vue/reactivity";
 import PlButton from "../../../src/packages/PlButton";
 import {DemoLine} from "../../components/DemoLine";
+import {PlInput} from "../../../src/packages/PlInput";
 
 export default designPage(() => {
 
@@ -19,6 +20,16 @@ export default designPage(() => {
                     v-model={val[18]}
                     foot={<PlButton mode={'text'} size={'mini'} onClick={setTime} label={'现在'}/>}
                 />
+                <PlInput style={{display: 'block'}}/>
+            </DemoRow>
+            <DemoRow title={'获取焦点'}>
+                <PlTime onFocus={() => console.log('focus')} onBlur={() => console.log('blue')}/>
+                <PlTime onFocus={() => console.log('focus')} onBlur={() => console.log('blue')} range/>
+            </DemoRow>
+            <DemoRow title={'基本用法'}>
+                <DemoLine>{val[0]}</DemoLine>
+                <PlTime v-model={val[0]}/>
+                <PlTime v-model={val[0]}/>
             </DemoRow>
         </div>
     )
