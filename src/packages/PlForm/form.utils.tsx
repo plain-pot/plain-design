c1871436a7605a5a0e388c2d8a1599d8082e1bc6
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-29 10:06:04 +0800

    components: add form

---
+++ src/packages/PlForm/form.utils.tsx
@@ -0,0 +1,19 @@
+export const enum FormLabelAlign {
+    left = 'left',
+    center = 'center',
+    right = 'right',
+}
+
+export const enum FormContentAlign {
+    'flex-start' = 'flex-start',
+    'flex-end' = 'flex-end',
+    'center' = 'center',
+    'space-around' = 'space-around',
+    'space-between' = 'space-between',
+    'space-evenly' = 'space-evenly',
+}
+
+export const enum FormValidateMode {
+    form = 'form',
+    table = 'table',
+}
