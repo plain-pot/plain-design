a8c13ec07cb02e484c89480702d2f65490464b78
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-29 11:47:35 +0800

    components: form basic

---
+++ story/pages/form/DemoFormGrid.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {designPage} from "plain-design-composition";
+import {DemoRow} from "../../components/DemoRow";
+
+export default designPage(() => {
+    return () => (
+        <div>
+            <DemoRow>
+
+            </DemoRow>
+        </div>
+    )
+})
