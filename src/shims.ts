2560b61d3af0ace1dd10ac47e9d14d2c96cfbd17
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-03-29 17:17:25 +0800

    components: add tree

--- src/shims.ts
+++ src/shims.ts
@@ -1,2 +1,6 @@
+import {ReactNode} from "react";
+
 export type RequireFormat<T, P extends keyof T> = Required<{ [k in P]: T[k] }> & { [k in Exclude<keyof T, P>]: T[k] }
 export type ModelType = { value: any }
+export type SimpleObject = Record<string, any>
+export type  VNodeChild = ReactNode
