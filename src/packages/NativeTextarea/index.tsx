4e016c87dd68ead0968bed87da468c4cea68c1bd
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-04-02 15:31:14 +0800

    design: remove fixInputCursor

--- src/packages/NativeTextarea/index.tsx
+++ src/packages/NativeTextarea/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
-import {fixInputCursor} from "../../utils/fixInputCursor";
+import {fixInputCursor} from "plain-design-composition";
 
 export const NativeTextarea = fixInputCursor<React.FC<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>>>('textarea' as any)
