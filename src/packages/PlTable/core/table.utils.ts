e01852ed2bf54683e9585ca6c8b36ad51b535f48
韦胜健 <martsforever@aliyun.com>
2021-05-09 22:05:54 +0800

    design: 调整表格的样式

--- src/packages/PlTable/core/table.utils.ts
+++ src/packages/PlTable/core/table.utils.ts
@@ -56,8 +56,8 @@ export const TableProps = {
     stripe: {type: Boolean},                                    // 是否为斑马纹table
     hideHeader: {type: Boolean},                                // 是否隐藏表头
     disabledStickyCompatible: {type: Boolean},                  // 是否禁用在ie下的sticky兼容
-    headRowHeight: {type: [String, Number], default: 45},       // 表头行高
-    bodyRowHeight: {type: [String, Number], default: 40},       // 表体行高
+    headRowHeight: {type: [String, Number], default: 40},       // 表头行高
+    bodyRowHeight: {type: [String, Number], default: 36},       // 表体行高
     showRows: {type: Number, default: 10},                      // 表格显示的行数，当表体的行数超过这个值时，将会出现表体内部滚动，这个属性实际上就是用来设值表格高度
 
     rowClassFunc: {type: Function as PropType<TablePropsRowClassFunc>},// 行 className 的计算函数
