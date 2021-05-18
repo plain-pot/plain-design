b11c4eddb0bc5604cf3c2e99d3dbefc548701d51
韦胜健 <martsforever@aliyun.com>
2021-05-18 22:53:44 +0800

    design: 准备实现useTableOption

--- story/pages/table-pro/basic-usage.tsx
+++ story/pages/table-pro/basic-usage.tsx
@@ -2,12 +2,15 @@ import {designPage, onMounted} from "plain-design-composition";
 import React from "react";
 import {env} from "../../env";
 import {http} from "../../http/http";
+import {useTableOption} from "./useTableOption";
 
 console.log(env)
 
 export default designPage(() => {
 
-
+    const option = useTableOption({
+        url: '',
+    })
 
     return () => <>
         hell world
---
+++ story/pages/table-pro/useTableOption.tsx
@@ -0,0 +1,20 @@
+import {createUseTableOption} from "../../../src";
+
+export const useTableOption = createUseTableOption({
+    keyField: 'id',
+    bodyRowHeight: 48,
+    headRowHeight: 48,
+    indexing: true,
+    border: false,
+    showRow: 10,
+    pageSizeOptions: [5, 10, 20, 50, 100],
+    editType: 'inline',
+    loadOnStart: true,
+    request: {} as any,
+    getDefaultUrlConfig: {} as any,
+    defaultNewRow: {},
+    copyDefaultExcludeKeys: ['id', 'createdBy', 'createdAt', 'updatedBy', 'updatedAt'],
+    injectRules: () => {
+
+    }
+})
