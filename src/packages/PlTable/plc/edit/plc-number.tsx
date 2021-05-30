1665a1fb35c2558efbdc8e4653df504bf3946655
韦胜健 <martsforever@aliyun.com>
2021-05-30 10:56:35 +0800

    table: 准备重写table

--- src/packages/PlTable/plc/edit/plc-number.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {designPlc} from "../core/designPlc";
-import {PlNumber} from "../../../PlNumber";
-
-export default designPlc({
-    name: 'plc-number',
-}, {
-    edit: ({row, plc}) => !plc.props.field ? null : <PlNumber v-model={row[plc.props.field]}/>
-},)
