7d2294d0b3cd9ac6b41abeff2ab8b42bf8713f82
韦胜健 <martsforever@aliyun.com>
2021-05-31 21:27:19 +0800

    table: 准备计算 plcData

--- src/packages/PlTable/table/Table.tsx
+++ src/packages/PlTable/table/Table.tsx
@@ -1,14 +1,28 @@
 import {designComponent, onMounted, useRefs} from "plain-design-composition";
 import React from "react";
 import {PlcGroup} from "../../PlcGroup";
+import {StyleProps, StyleShape, StyleSize, useStyle} from "../../../use/useStyle";
+import {EditProps} from "../../../use/useEdit";
 
 export default designComponent({
     name: 'pl-table',
+    props: {
+        ...EditProps,
+        ...StyleProps,
+    },
     provideRefer: true,
     slots: ['default'],
     setup({props, slots}) {
 
         const {refs, onRef} = useRefs({group: PlcGroup})
+
+        const {styleComputed} = useStyle({
+            adjust: config => {
+                config.shape = props.shape as any || StyleShape.square
+                config.size = props.size as any || StyleSize.normal
+                config.status = props.status as any
+            }
+        })
 
         onMounted(() => {
             // console.log(refs.group!.items.value)
