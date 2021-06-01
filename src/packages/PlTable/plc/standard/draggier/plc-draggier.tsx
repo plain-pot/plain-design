5a8be4c1ed541e423feb2a6321c8f9ef79c2dccb
韦胜健 <martsforever@aliyun.com>
2021-06-01 21:05:07 +0800

    table: plc-draggier

--- src/packages/PlTable/plc/standard/draggier/plc-draggier.tsx
+++ src/packages/PlTable/plc/standard/draggier/plc-draggier.tsx
@@ -1,46 +1,53 @@
-import {designPlc} from "../../core/designPlc";
 import {useListDraggier} from "./core";
 import {ie} from "plain-utils/utils/ie";
 import {useTableGetScroll} from "../../../table/use/useTableGetScroll";
 import {injectPlainTable} from "../../../index";
-import {computed} from "plain-design-composition";
+import {computed, designComponent} from "plain-design-composition";
 import PlButton from "../../../../PlButton";
 import React from "react";
+import {createPlcPropOptions, PlcEmitsOptions} from "../../utils/plc.utils";
+import {PlcScopeSlotsOptions} from "../../utils/plc.scope-slots";
+import {useExternalPlc} from "../../core/useExternalPlc";
 
-export default designPlc({
+export default designComponent({
     name: 'plc-draggier',
-    standardProps: {
-        autoFixedLeft: {default: true},
-        order: {default: -9998},
-        width: {default: 40},
-        align: {default: 'center'},
-        noPadding: {type: Boolean, default: true},
-    },
-    setup: (props) => {
-        const {disabledVirtual, dataModel, event} = injectPlainTable()
-        const {getScroll} = useTableGetScroll(event.on.onVirtualMounted)
+    props: createPlcPropOptions({
+        autoFixedLeft: true,
+        order: -9998,
+        width: 40,
+        align: 'center',
+        noPadding: true,
+    }),
+    emits: PlcEmitsOptions,
+    scopeSlots: PlcScopeSlotsOptions,
+    setup({props, scopeSlots, event}) {
+        const table = injectPlainTable()
+        const {getScroll} = useTableGetScroll(table.event.on.onVirtualMounted)
         const handler = computed(() => useListDraggier({
-            virtual: !disabledVirtual.value && !ie,
+            virtual: !table.disabledVirtual.value && !ie,
             // virtual: true,
             rowClass: 'plt-row',
             getScroll,
             onChange: async (start, end) => {
-                dataModel.value!.splice(end, 0, dataModel.value!.splice(start, 1)[0])
+                table.dataModel.value!.splice(end, 0, table.dataModel.value!.splice(start, 1)[0])
             },
         }))
-        return {
-            onMouseDown: handler.value.handler.mousedown,
-            onClick: (e: React.MouseEvent) => {
-                e.stopPropagation()
-                e.preventDefault()
-            },
+
+        const onClick = (e: React.MouseEvent) => {
+            e.stopPropagation()
+            e.preventDefault()
         }
-    }
-}, {
-    default: ({refer}) => <PlButton
-        icon="el-icon-list"
-        mode="text"
-        className="plc-draggier-handler"
-        {...refer}
-    />
+
+        return useExternalPlc({
+            props, scopeSlots, event, defaultScopeSlots: {
+                default: () => <PlButton
+                    icon="el-icon-list"
+                    mode="text"
+                    className="plc-draggier-handler"
+                    onMouseDown={handler.value.handler.mousedown}
+                    onClick={onClick}
+                />
+            }
+        })
+    },
 })
