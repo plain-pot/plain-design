4a91c5123c7471aa29cf03bd1385a264342bdb5d
韦胜健 <martsforever@aliyun.com>
2021-06-03 19:15:04 +0800

    design: 准备实现plc 的onClick

--- src/packages/PlTable/plc/utils/plc.utils.tsx
+++ src/packages/PlTable/plc/utils/plc.utils.tsx
@@ -9,7 +9,7 @@ export type PlcPropsEditable = boolean | ((node: TableNode) => boolean)
 
 export const PlcGroupPropsOptions = {
     title: {type: String},                                                  // 列标题
-    align: {type: String as PropType<string | TablePlcAlign>},                       // 非编辑状态下文本对其方式
+    align: {type: String as PropType<string | TablePlcAlign>},              // 非编辑状态下文本对其方式
     noPadding: {type: Boolean},                                             // 是否不兼容表格的虚拟滚动功能
     colDraggable: {type: Boolean, default: null},                           // 列是否可以拖拽排序
 
