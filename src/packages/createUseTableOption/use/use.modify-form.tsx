5a72fa57e49c63f58e48d30388ec202fc0809ef8
韦胜健 <martsforever@aliyun.com>
2021-06-05 21:03:25 +0800

    design: 批量修改对话框

--- src/packages/createUseTableOption/use/use.modify-form.tsx
+++ src/packages/createUseTableOption/use/use.modify-form.tsx
@@ -20,20 +20,23 @@ export function useTableOptionModifyForm() {
     const $dialog = useDialog()
     const {refs, onRef} = useRefs({form: PlForm})
 
-    const modify = ({node, title, plcList, onConfirm, onCancel, rules}: {
-        node: TableNode,
+    const modify = ({modifyNode, title, plcList, onConfirm, onCancel, rules}: {
+        modifyNode: TableNode,
         title: string,
         plcList: tPlc[],
         onConfirm: (node: TableNode) => void,
         onCancel?: () => void,
         rules?: tFormPropRules,
     }) => {
-        node.edit = true
-        node.editRow = node.data
         plcList = plcList.filter(i => !i.props.hideInForm && !!i.props.field)
         const state = reactive({
             editPlcArr: [plcList[0]] as tPlc[],
             toBeAddPlc: plcList[1] as tPlc | undefined,
+            node: {
+                ...modifyNode,
+                editRow: modifyNode.data,
+                edit: true,
+            },
         })
 
         const addPlcList = computed(() => plcList.filter(i => i !== state.toBeAddPlc && state.editPlcArr.indexOf(i) === -1))
@@ -47,9 +50,10 @@ export function useTableOptionModifyForm() {
                 !!state.toBeAddPlc && state.editPlcArr.push(state.toBeAddPlc)
                 state.toBeAddPlc = addPlcList.value[0]
             },
-            rmPlc: (index: number) => {
+            rmPlc: (plc: tPlc, index: number) => {
                 state.editPlcArr.length > 1 && state.editPlcArr.splice(index, 1)
                 state.toBeAddPlc = addPlcList.value[0]
+                delete state.node.editRow[plc.props.field!]
             },
         }
 
@@ -75,7 +79,7 @@ export function useTableOptionModifyForm() {
                             column={1}
                             width={'100%'}
                             centerWhenSingleColumn={false}
-                            modelValue={node.editRow}
+                            modelValue={state.node.editRow}
                             rules={rules}
                             style={{overflow: 'hidden'}}
                         >
@@ -85,10 +89,12 @@ export function useTableOptionModifyForm() {
                                     label={plc.props.title}
                                     required={plc.props.required}
                                     rules={plc.props.rules}
-                                    field={plc.props.field}>
+                                    field={plc.props.field}
+                                    contentAlign="space-between"
+                                >
                                     {{
-                                        default: renderBodyCell({node, plc, formEdit: true}).body,
-                                        suffix: <PlButton icon="el-icon-minus" mode="stroke" onClick={() => handler.rmPlc(index)}/>
+                                        default: renderBodyCell({node: state.node, plc, formEdit: true}).body,
+                                        suffix: <PlButton icon="el-icon-minus" mode="stroke" onClick={() => handler.rmPlc(plc, index)}/>
                                     }}
                                 </PlFormItem>
                             ))}
@@ -123,7 +129,7 @@ export function useTableOptionModifyForm() {
             onConfirm: async () => {
                 await refs.form!.validate()
                 dialog.close()
-                onConfirm(node)
+                onConfirm(state.node)
             },
             onCancel: () => {
                 dialog.close()
