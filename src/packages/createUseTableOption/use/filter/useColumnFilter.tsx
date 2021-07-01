122732ea8fe9d154a7a9b37a454f20f6be4d20e6
韦胜健 <martsforever@aliyun.com>
2021-07-01 21:30:36 +0800

    design: 能用 $$contextmenu 服务吗

--- src/packages/createUseTableOption/use/filter/useColumnFilter.tsx
+++ src/packages/createUseTableOption/use/filter/useColumnFilter.tsx
@@ -1,20 +1,53 @@
 import {tTableOptionHooks} from "../use.hooks";
 import {reactive} from "plain-design-composition";
 import {tPlc} from "../../../PlTable/plc/utils/plc.type";
+import {FilterConfig, iFilterTargetOption} from "../../../PlFilter/FilterConfig";
+
+interface ColumnFilterData {
+    desc: null | boolean,
+    fto: iFilterTargetOption,
+    distinctFilterValues: null | string[],
+}
 
 export function useColumnFilter({hooks}: { hooks: tTableOptionHooks }) {
+
+    const getColumnKey = (plc: tPlc) => plc.props.field! + (plc.props.title || '#_#')
+
     const state = reactive({
         getSourceFlatPlcList: null as null | (() => tPlc[]),
+        columnFilterDataMap: {} as Record<string, ColumnFilterData>,
+    })
+    hooks.onCollectPlcData.use(val => {
+        const flatPlcList = val.sourceFlatPlcList.filter(i => !!i.props.field)
+        state.getSourceFlatPlcList = (() => flatPlcList)
+        const oldColumnFilterDataMap = state.columnFilterDataMap
+        state.columnFilterDataMap = flatPlcList.reduce((prev, plc) => {
+            const key = getColumnKey(plc)
+            const oldColumnFilterData = oldColumnFilterDataMap[key]
+            prev[key] = !!oldColumnFilterData ? {...oldColumnFilterData} : {
+                desc: null,
+                distinctFilterValues: null,
+                fto: FilterConfig.getTargetOption({
+                    label: plc.props.title!,
+                    field: plc.props.field!,
+                    value: null,
+                    filterName: plc.props.filterName,
+                    handlerName: plc.props.filterHandler,
+                    filterConfig: plc.props.filterConfig,
+                    plc,
+                })!,
+            }
+            return prev
+        }, {} as Record<string, ColumnFilterData>)
     })
 
-    hooks.onCollectPlcData.use(val => { state.getSourceFlatPlcList = (() => val.sourceFlatPlcList)})
-
     hooks.onClickHead.use(({plc, e}) => {
-        if (plc.group) {
-            /*分组表头不做处理, 仅处理列表头*/
-            return
-        }
-        state.getSourceFlatPlcList!().forEach(i => console.log({...i.props}))
+        /*分组表头不做处理, 仅处理列表头*/
+        if (plc.group) {return}
+        const columnKey = getColumnKey(plc)
+        const columnFilterData = state.columnFilterDataMap[columnKey]
+        if (!columnFilterData) {return;}
+        console.log(columnFilterData)
     })
 
     return {
