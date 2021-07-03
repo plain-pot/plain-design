ee56dd34da6a891723dc5457341051385b4f96ef
韦胜健 <martsforever@aliyun.com>
2021-07-03 11:47:16 +0800

    design: 列搜索的筛选以及排序功能

--- src/packages/createUseTableOption/use/filter/useColumnFilter.tsx
+++ src/packages/createUseTableOption/use/filter/useColumnFilter.tsx
@@ -1,5 +1,5 @@
 import {tTableOptionHooks} from "../use.hooks";
-import {computed, reactive} from "plain-design-composition";
+import {classnames, computed, reactive} from "plain-design-composition";
 import {tPlc} from "../../../PlTable/plc/utils/plc.type";
 import {FilterConfig, iFilterOption, iFilterQuery, iFilterTargetOption} from "../../../PlFilter/FilterConfig";
 import useContextmenu from "../../../useContextmenu";
@@ -13,16 +13,17 @@ import {toArray} from "../../../../utils/toArray";
 
 interface ColumnFilterData {
     desc: null | boolean,
+    sortIndex: null | number,                   // 每次对某个字段进行排序，都会变成最高优先级的排序功能
     option: iFilterOption,
     distinctFilterValues: null | string[],
 }
 
-interface ColumnFilterTargetData {
-    desc: null | boolean,
-    option: iFilterOption,
+interface ColumnFilterTargetData extends ColumnFilterData {
     fto?: iFilterTargetOption,
-    distinctFilterValues: null | string[],
+    sourceData: () => ColumnFilterData,
 }
+
+const MAX_SORT_INDEX = 100
 
 export function useColumnFilter({hooks, methods}: { hooks: tTableOptionHooks, methods: tTableOptionMethods }) {
 
@@ -38,7 +39,8 @@ export function useColumnFilter({hooks, methods}: { hooks: tTableOptionHooks, me
     const columnFilterTargetDataMap = computed(() => Object.entries(state.columnFilterDataMap).reduce((prev, [columnKey, cfd]) => {
         prev[columnKey] = {
             ...cfd,
-            fto: FilterConfig.getTargetOption(cfd.option)
+            fto: FilterConfig.getTargetOption(cfd.option),
+            sourceData: () => cfd,
         }
         return prev
     }, {} as Record<string, ColumnFilterTargetData>))
@@ -52,6 +54,7 @@ export function useColumnFilter({hooks, methods}: { hooks: tTableOptionHooks, me
             const oldColumnFilterData = oldColumnFilterDataMap[key]
             prev[key] = !!oldColumnFilterData ? {...oldColumnFilterData} : {
                 desc: null,
+                sortIndex: null,
                 distinctFilterValues: null,
                 option: {
                     label: plc.props.title!,
@@ -79,29 +82,61 @@ export function useColumnFilter({hooks, methods}: { hooks: tTableOptionHooks, me
         return !!queries && queries.length > 0 ? [...data, {queries: toArray(queries),}] : data
     })
 
+    const minSortIndex = computed(() => Object.values(state.columnFilterDataMap).reduce((prev, item) => Math.min(prev, item.sortIndex || MAX_SORT_INDEX), MAX_SORT_INDEX))
+
+    const changeSort = (cftd: ColumnFilterTargetData, desc: boolean) => {
+        const cfd = cftd.sourceData()
+        if (cfd.desc == null || cfd.desc !== desc) {
+            cfd.desc = desc
+            cfd.sortIndex = minSortIndex.value - 1
+        } else {
+            cfd.desc = null
+            cfd.sortIndex = null
+        }
+        methods.pageMethods.reload()
+    }
+
+    hooks.onCollectSortData.use((prev) => {
+        const sortData = Object.values(columnFilterTargetDataMap.value)
+            .filter(i => !!i.option.field && i.sortIndex != null && i.desc != null)
+            .map(i => ({field: i.option.field, desc: i.desc, sortIndex: i.sortIndex}))
+            .sort((a, b) => -(a.sortIndex! - b.sortIndex!)) as { field: string, desc: boolean, sortIndex: number }[]
+        if (sortData.length === 0) {return prev}
+        return [...(sortData.map(i => ({field: i.field, desc: i.desc}))), ...prev,]
+    })
+
     hooks.onClickHead.use(({plc, e}) => {
         /*分组表头不做处理, 仅处理列表头*/
-        if (plc.group) {return}
+        if (plc.group || !plc.props.field) {return}
         const menuOpt = {} as any
 
         $contextmenu(e.currentTarget, () => {
             const columnKey = getColumnKey(plc)
             const columnFilterTargetData = columnFilterTargetDataMap.value[columnKey]
             if (!columnFilterTargetData) {return;}
+
+            const {fto, desc} = columnFilterTargetData
+
             return <>
                 <div onClick={e => e.stopPropagation()} className="pro-column-filter-container">
                     <div className="pro-column-filter-sort-container">
-                        <div className="pro-column-filter-sort-item pro-column-filter-sort-item-active">
+                        <div className={classnames([
+                            'pro-column-filter-sort-item',
+                            {'pro-column-filter-sort-item-active': desc !== null && !desc}
+                        ])} onClick={() => changeSort(columnFilterTargetData, false)}>
                             <PlIcon icon="el-icon-upload1"/>
                             <span>升序</span>
                         </div>
-                        <div className="pro-column-filter-sort-item">
+                        <div className={classnames([
+                            'pro-column-filter-sort-item',
+                            {'pro-column-filter-sort-item-active': desc !== null && !!desc}
+                        ])} onClick={() => changeSort(columnFilterTargetData, true)}>
                             <PlIcon icon="el-icon-download"/>
                             <span>降序</span>
                         </div>
                     </div>
                     <div>
-                        <PlFilter fto={columnFilterTargetData.fto} hideSearchButton onConfirm={methods.pageMethods.reload}/>
+                        <PlFilter fto={fto} hideSearchButton onConfirm={methods.pageMethods.reload}/>
                     </div>
                     <div>
                         <PlButton mode="stroke" icon="el-icon-thumb" label="关闭" onClick={() => menuOpt.hide()}/>
