4d95692c3b1cf8889b59125bcd34f11f02664b2a
韦胜健 <martsforever@aliyun.com>
2021-07-17 09:55:36 +0800

    design: table buttons

--- src/packages/createUseTableOption/use/filter/use.filter.tsx
+++ src/packages/createUseTableOption/use/filter/use.filter.tsx
@@ -4,14 +4,15 @@ import {tTableOptionMethods} from "../use.methods";
 import {useSearchFilter} from "./useSearchFilter";
 import {useFormFilter} from "./useFormFilter";
 import {useColumnFilter} from "./useColumnFilter";
+import {iTableProConfig} from "../../createUseTableOption.utils";
 
-export function useTableOptionFilter({hooks, methods}: { hooks: tTableOptionHooks, methods: tTableOptionMethods, }) {
+export function useTableOptionFilter({hooks, methods, customConfig}: { hooks: tTableOptionHooks, methods: tTableOptionMethods, customConfig: iTableProConfig }) {
 
     const formFilter = useFormFilter({hooks, methods})
 
     const searchFilter = useSearchFilter({hooks, methods, onCollapse: () => formFilter.toggle(), isCollapse: () => !formFilter.state.isShow})
 
-    const columnFilter = useColumnFilter({hooks, methods})
+    const columnFilter = useColumnFilter({hooks, methods, customConfig})
 
     return {
         searchFilter,
