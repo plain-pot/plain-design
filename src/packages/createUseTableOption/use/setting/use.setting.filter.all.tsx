2f86b58c3403179a5d54fceae703d1b7302b221f
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-07-21 11:28:59 +0800

    table: 清空某个筛选类型之后，移除其渲染内容

--- src/packages/createUseTableOption/use/setting/use.setting.filter.all.tsx
+++ src/packages/createUseTableOption/use/setting/use.setting.filter.all.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {eTableOptionSettingView, iTableOptionSettingInnerUser} from "./use.setting.utils";
-import {tTableOptionFilter} from "../use.filter.state";
+import {FilterStateInitialization, tTableOptionFilter} from "../use.filter.state";
 import './use.setting.filter.all.scss'
 import PlButton from "../../../PlButton";
 import {reactive} from "plain-design-composition";
@@ -8,8 +8,13 @@ import {reactive} from "plain-design-composition";
 export function useTableOptionSettingAllFilter({useTableOptionSettingInner, filterState}: { useTableOptionSettingInner: iTableOptionSettingInnerUser, filterState: tTableOptionFilter }) {
 
     const state = reactive({
-        render: () => <></>,
+        renderFilters: [] as FilterStateInitialization[],
     })
+
+    const removeFilter = (filter: FilterStateInitialization) => {
+        state.renderFilters.splice(state.renderFilters.indexOf(filter), 1)
+        filterState.clearFilter(filter)
+    }
 
     useTableOptionSettingInner({
         key: eTableOptionSettingView.allFilter,
@@ -17,28 +22,26 @@ export function useTableOptionSettingAllFilter({useTableOptionSettingInner, filt
         seq: 0.9,
         contentPending: false,
         beforeOpen: () => {
-            const content = filterState.state.filters.filter(i => i.getActiveFilterCount() > 0)
-                .map((filter, index) => {
-                    const display = filter.display()
-                    if (!display) {return null}
-                    return <div key={index} className="pl-table-pro-setting-all-filter-item">
-                        <div className="pl-table-pro-setting-all-filter-item-head">
-                            <span>{filter.title}</span>
-                            <PlButton icon="el-icon-delete" label="清空" status="error" onClick={() => filterState.clearFilter(filter)} mode="text"/>
-                        </div>
-                        <div className="pl-table-pro-setting-all-filter-item-body">
-                            {filter.display()}
-                        </div>
-                    </div>
-                })
-            state.render = () => <>{content}</>
+            state.renderFilters = filterState.state.filters.filter(i => i.getActiveFilterCount() > 0)
         },
         render: () => (
             <div className="pl-table-pro-setting-all-filter">
                 <div style={{display: 'inline-block', width: '100%'}}>
                     <PlButton label="全部清空" icon="el-icon-delete" status="error" style={{float: 'right'}} onClick={filterState.clearAll}/>
                 </div>
-                {state.render()}
+                {state.renderFilters.map((filter, index) => {
+                    const display = filter.display()
+                    if (!display) {return null}
+                    return <div key={index} className="pl-table-pro-setting-all-filter-item">
+                        <div className="pl-table-pro-setting-all-filter-item-head">
+                            <span>{filter.title}</span>
+                            <PlButton icon="el-icon-delete" label="清空" status="error" onClick={() => removeFilter(filter)} mode="text"/>
+                        </div>
+                        <div className="pl-table-pro-setting-all-filter-item-body">
+                            {filter.display()}
+                        </div>
+                    </div>
+                })}
             </div>
         )
     })
