ab3edb9baf2e67ab9d307f145d8b595e90d71bd3
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-07-22 10:23:23 +0800

    table: 准备实现高级筛选，先解决高级排序显示的问题

--- src/packages/createUseTableOption/use/setting/use.setting.filter.senior.tsx
+++ src/packages/createUseTableOption/use/setting/use.setting.filter.senior.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import {eTableOptionSettingView, iTableOptionSettingInnerUser} from "./use.setting.utils";
+import {reactive} from "plain-design-composition";
+import {iFilterOption} from "../../../PlFilter/FilterConfig";
+
+
+interface iSeniorFilterData extends iFilterOption {
+    id: string
+}
 
 export function useTableOptionSettingSeniorFilter({useTableOptionSettingInner}: { useTableOptionSettingInner: iTableOptionSettingInnerUser }) {
+
+    const state = reactive({
+        seniorList: [] as iSeniorFilterData[],
+        operator: null as null | string,
+    })
+
     useTableOptionSettingInner({
         key: eTableOptionSettingView.seniorFilter,
         title: '高级筛选',
