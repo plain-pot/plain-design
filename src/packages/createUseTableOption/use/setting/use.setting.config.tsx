90677777b7f1be73aeb4e6b26f93a5824470edaa
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-07-23 10:04:09 +0800

    table: 怎么实现编辑了之后设置顺序的问题呢

--- src/packages/createUseTableOption/use/setting/use.setting.config.tsx
+++ src/packages/createUseTableOption/use/setting/use.setting.config.tsx
@@ -7,6 +7,10 @@ import {Plc} from "../../../Plc";
 import {PlcIndex} from "../../../PlcIndex";
 import {PlcDraggier} from "../../../PlcDraggier";
 import {reactive} from "plain-design-composition";
+import {PlcSelect} from "../../../PlcSelect";
+import PlSelectOption from "../../../PlSelectOption";
+import {PlcCheckbox} from "../../../PlcCheckbox";
+import {PlcNumber} from "../../../PlcNumber";
 
 export function useTableOptionSettingConfig(
     {
@@ -30,17 +34,23 @@ export function useTableOptionSettingConfig(
                 ...i.getState(),
                 title: i.props.title,
             }))
+            console.log(deepcopy(state.data))
         },
         render: () => (
             <div>
-                <PlTable data={state.data} showRows={Math.max(5, state.data.length)}>
+                <PlTable data={state.data} showRows={Math.max(5, state.data.length)} defaultEditingWhenAddRow>
                     <PlcIndex/>
                     <PlcDraggier/>
                     <Plc title="标题" field="title"/>
-                    <Plc title="对齐方式" field="align"/>
-                    <Plc title="固定" field="fixed"/>
-                    <Plc title="宽度" field="width"/>
-                    <Plc title="隐藏" field="hidden"/>
+                    <PlcSelect title="对齐方式" field="align">
+                        <PlSelectOption label="左对齐" val="left"/>
+                        <PlSelectOption label="居中对齐" val="center"/>
+                        <PlSelectOption label="右对齐" val="right"/>
+                    </PlcSelect>
+                    <PlcNumber title="宽度" field="width"/>
+                    <PlcCheckbox title="固定" width={50} align="center" field="fixed" trueValue="left" falseValue={undefined}/>
+                    <PlcCheckbox title="隐藏" width={50} align="center" field="hidden" trueValue={true} falseValue={undefined}/>
+                    <Plc title={' '} fit/>
                 </PlTable>
             </div>
         )
