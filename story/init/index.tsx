0dc970b779cf3573b24c66903fdcd6dc3e5711b9
韦胜健 <martsforever@aliyun.com>
2021-08-03 21:47:45 +0800

    useHttp

--- story/init/index.tsx
+++ story/init/index.tsx
@@ -1,8 +1,10 @@
 import {initialize} from '../../src/index'
 import useTableOption from "./useTableOption";
 import useObjectOption from "./useObjectOption";
+import {useHttp} from "./useHttp";
 
 initialize(() => ({
     useTableOption,
     useObjectOption,
+    useHttp,
 }))
