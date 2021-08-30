2eb5df5616315e8c2176ddc124ea319c26a362da
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-08-30 11:24:27 +0800

    table: adjust demo tabs

--- story/pages/normal/DemoTab.tsx
+++ story/pages/normal/DemoTab.tsx
@@ -1,7 +1,75 @@
-import {designPage} from "plain-design-composition";
+import {designPage, reactive} from "plain-design-composition";
 import React from "react";
-import {PlInput, PlTab, PlTabs} from "../../../src";
+import {PlRadioGroup, PlRadio, PlInput, PlTab, PlTabs} from "../../../src";
 import {DemoRow} from "../../components/DemoRow";
+import {createRpcReturn} from "fork-ts-checker-webpack-plugin/lib/rpc";
+import {DemoLine} from "../../components/DemoLine";
+
+export const DemoBasic = designPage(() => {
+    return () => (
+        <DemoRow title="基本用法">
+            <PlTabs headType="text">
+                <PlTab title="用户管理">
+                    <div style={{height: '100px', padding: '20px 0'}}>
+                        用户管理：
+                        <PlInput onMounted={() => console.log('111 mounted')}/>
+                    </div>
+                </PlTab>
+                <PlTab title="配置管理">
+                    <div style={{height: '200px', padding: '20px 0'}}>
+                        配置管理：
+                        <PlInput onMounted={() => console.log('222 mounted')}/>
+                    </div>
+                </PlTab>
+                <PlTab title="数据集管理">
+                    <div style={{height: '300px', padding: '20px 0'}}>
+                        数据集管理：
+                        <PlInput onMounted={() => console.log('333 mounted')}/>
+                    </div>
+                </PlTab>
+            </PlTabs>
+        </DemoRow>
+    )
+})
+
+export const DemoDirection = designPage(() => {
+    const state = reactive({
+        position: 'top'
+    })
+
+    return () => (
+        <DemoRow title="方向">
+            <DemoLine>
+                <PlRadioGroup v-model={state.position}>
+                    <PlRadio val="top" label="top"/>
+                    <PlRadio val="bottom" label="bottom"/>
+                    <PlRadio val="left" label="left"/>
+                    <PlRadio val="right" label="right"/>
+                </PlRadioGroup>
+            </DemoLine>
+            <PlTabs headPosition={state.position as any}>
+                <PlTab title="用户管理">
+                    <div style={{height: '100px', padding: '20px 0'}}>
+                        用户管理：
+                        <PlInput onMounted={() => console.log('111 mounted')}/>
+                    </div>
+                </PlTab>
+                <PlTab title="配置管理">
+                    <div style={{height: '200px', padding: '20px 0'}}>
+                        配置管理：
+                        <PlInput onMounted={() => console.log('222 mounted')}/>
+                    </div>
+                </PlTab>
+                <PlTab title="数据集管理">
+                    <div style={{height: '300px', padding: '20px 0'}}>
+                        数据集管理：
+                        <PlInput onMounted={() => console.log('333 mounted')}/>
+                    </div>
+                </PlTab>
+            </PlTabs>
+        </DemoRow>
+    )
+})
 
 export const demoText = designPage(() => {
     return () => (
@@ -161,7 +229,7 @@ export const demoVertical = designPage(() => {
     return () => (
         <DemoRow title="vertical">
             {['left', 'right'].map(pos => (
-                <PlTabs headPosition={pos as any} key={pos} style={{width: '49%', display: 'inline-flex', verticalAlign: 'top'}}>
+                <PlTabs headPosition={pos as any} key={pos} style={{width: 'calc((100% - 32px)/2)', display: 'inline-flex', verticalAlign: 'top'}}>
                     <PlTab title="用户管理">
                         <div style={{height: '100px'}}>
                             user management
