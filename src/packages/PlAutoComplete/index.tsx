51c46ed1e4ed486e4d408b7485b3572348f5283a
韦胜健 <martsforever@aliyun.com>
2021-10-25 23:40:47 +0800

    build: 完成AutoComplete的基本输入功能

--- src/packages/PlAutoComplete/index.tsx
+++ src/packages/PlAutoComplete/index.tsx
@@ -1,23 +1,169 @@
-import {designComponent, useModel} from "plain-design-composition";
+import {classnames, computed, designComponent, PropType, reactive, useModel, useRefs} from "plain-design-composition";
 import React from "react";
 import PlInput from "../PlInput";
+import {useEditPopperAgent} from "../useEditPopperAgent/useEditPopperAgent";
+import {useSelect} from "../PlSelect/useSelect";
+import {PlSelectPanel} from "../PlSelect/PlSelectPanel";
+import PlPopper from "../PlPopper";
+import {handleKeyboard} from "../keyboard";
+import PlSelectOption from "../PlSelectOption";
 
 export const PlAutoComplete = designComponent({
     props: {
         modelValue: {type: [String, Number]},
+        suggestion: {type: Array as PropType<string[]>},
+        filterMethod: Function,                                         // 筛选过滤函数
+        popperAttrs: {type: Object as PropType<Partial<typeof PlPopper.use.props>>},
+        inputProps: {type: Object as PropType<Partial<typeof PlInput.use.props>>},                                     // input组件绑定属性对象
     },
     emits: {
         onUpdateModelValue: (val?: string | number) => true,
+        onInputChange: (val: string | null) => true,
+        onClick: (val: string | number) => true,
+        onBlur: (e: React.FocusEvent) => true,
+        onFocus: (e: React.FocusEvent) => true,
     },
-    setup({props, event: {emit}}) {
+    slots: ['default'],
+    setup({props, event, slots}) {
+
+        const {emit} = event
+
+        const {refs, onRef} = useRefs({input: PlInput})
 
         const model = useModel(() => props.modelValue, emit.onUpdateModelValue)
 
+        /*对 pl-select-panel 的引用*/
+        let panel = null as typeof PlSelectPanel.use.class | null
+
+        /**
+         * popper应该的高度
+         * @author  韦胜健
+         * @date    2020/12/4 10:06
+         */
+        const popperHeight = computed(() => !props.suggestion?.length || props.suggestion?.length > 6 ? 200 : null)
+
+        /*---------------------------------------utils-------------------------------------------*/
+
+        const utils = {
+            filterMethod: (option: { label?: string, val: string, disabled: boolean }) => {
+                if (!!props.filterMethod) return props.filterMethod(model.value, option)
+                return !!model.value && !!String(model.value).trim() ? (!!option.label && option.label.indexOf(String(model.value == null ? '' : model.value)) > -1) : true
+            }
+        }
+
+        const panelContentRender = (() => {
+            const state = reactive({
+                render: () => slots.default(props.suggestion?.map((suggestMsg, index) => (
+                    <PlSelectOption label={suggestMsg} val={suggestMsg} key={index}/>
+                ))),
+                reset: () => (state.render = () => slots.default())
+            })
+            return state
+        })();
+
+        /**
+         * 给input组件传递的属性
+         * @author  韦胜健
+         * @date    2020/12/4 11:21
+         */
+        const inputProps = computed(() => Object.assign({}, props.inputProps || {}))
+
+        const agentState = useEditPopperAgent({
+            event,
+            serviceGetter: useSelect,
+            option: {
+                reference: () => refs.input?.refs.input,
+                renderAttrs: () => ({
+                    ref: r => panel = r,
+                    height: popperHeight.value,
+                    content: panelContentRender.render,
+                    filterMethod: utils.filterMethod,
+                    onChange: handler.onServiceChange,
+                    onClick: event.emit.onClick,
+                }),
+                popperAttrs: ({
+                    ...props.popperAttrs as any,
+                    onMousedownPopper: () => agentState.state.focusCounter++,
+                    onClickPopper: () => refs.input!.methods.focus(),
+                    // onHide: () => model.value = null,
+                }),
+            },
+        })
+
+        const handler = {
+            onInputKeydown: handleKeyboard({
+                enter: (e) => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    if (agentState.isShow.value) {
+                        panel!.methods.selectHighlight()
+                    } else {
+                        agentState.methods.show()
+                    }
+                },
+                up: (e) => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    if (!!agentState.isShow.value) {
+                        panel!.methods.highlightPrev()
+                    }
+                },
+                down: (e) => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    if (!!agentState.isShow.value) {
+                        panel!.methods.highlightNext()
+                    }
+                },
+                esc: (e) => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    if (!!agentState.isShow.value) {
+                        agentState.methods.hide()
+                    }
+                },
+            }),
+            onServiceChange: (val: any) => model.value = val,
+            onInputClear: () => model.value = undefined,
+            onInputChange: (val: string | null) => {
+                model.value = val!
+                event.emit.onInputChange(val)
+                if (!agentState.isShow.value) {
+                    agentState.methods.show()
+                }
+            },
+        }
+
+        /**
+         * 给input组件绑定的目标对象
+         * @author  韦胜健
+         * @date    2020/12/4 11:21
+         */
+        const inputBinding = computed(() => {
+            /*onEnter不要了，只要剩余的时间监听器*/
+            const {onEnter, ...inputHandler} = agentState.inputHandler
+            return {
+                ref: onRef.input,
+                className: classnames([
+                    'pl-autocomplete',
+                ]),
+                modelValue: model.value,
+                suffixIcon: 'el-icon-arrow-down',
+                clearIcon: true,
+                isFocus: agentState.state.focusCounter > 0,
+                clearHandler: handler.onInputClear,
+
+                ...inputHandler,
+                onChange: handler.onInputChange,
+                onKeydown: handler.onInputKeydown,
+
+                ...inputProps.value,
+            }
+        })
+
         return {
             render: () => (
-                <PlInput
-                    v-model={model.value}
-                />
+                <PlInput{...inputBinding.value}/>
             )
         }
     },
--- story/pages/normal/DemoAutoComplete.tsx
+++ story/pages/normal/DemoAutoComplete.tsx
@@ -2,14 +2,18 @@ import {designPage, reactive} from "plain-design-composition";
 import {DemoRow} from "../../components/DemoRow";
 import React from "react";
 import {PlAutoComplete} from "../../../src";
+import addressData from '../data/address.json'
 
 export const demo1 = designPage(() => {
     const state = reactive({
         text: ''
     })
+
+    const suggestion = (addressData as any[]).map(({name}) => (name))
+
     return () => (
         <DemoRow title="基本用法">
-            <PlAutoComplete v-model={state.text}/>
+            <PlAutoComplete v-model={state.text} suggestion={suggestion}/>
             {state.text}
         </DemoRow>
     )
